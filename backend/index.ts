ff7a96af0c60cea6a50633d2fe3f50864f87a2fe
hendrixjoseph <hendixjoseph@aol.com>
2023-05-09 11:32:43 -0400

    start build process

--- backend/index.ts
+++ backend/index.ts
@@ -2,14 +2,20 @@ import greenlock from 'greenlock-express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import getApp from './app.js';
+import express from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const __parent = path.resolve(__dirname, '../');
+
+const app = getApp();
+
+app.use(express.static('../frontend'));
+app.use(express.static('../common'));
 
 greenlock.init({
   packageRoot: __parent,
   maintainerEmail: "contact@favemarx.com",
   configDir: './greenlock.d',
   cluster: false
-}).serve(getApp());
+}).serve(app);
